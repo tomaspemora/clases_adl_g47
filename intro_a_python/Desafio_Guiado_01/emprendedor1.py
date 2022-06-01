d87d4f4792271f62f92a16301316d48438ed51b7
tomaspemora <tomaspemora@gmail.com>
2022-05-31 23:52:09 -0400

    saque carpetas que no eran de clases

--- intro_a_python/Desafio_Guiado_01/emprendedor1.py
+++
@@ -1,6 +0,0 @@
-
-P = int(input("Ingrese el Precio de suscripción: "))
-U = int(input("Ingrese el Número de Usuarios: "))
-GT = int(input("Ingrese los Gastos totales: "))
-utilidades = P * U - GT
-print(f'La utilidad es: {utilidades}')
--- intro_a_python/Desafio_Guiado_01/escape.py
+++
@@ -1,5 +0,0 @@
-import math
-g = float(input("Ingrese el valor de g: "))
-r = int(input("Ingrese el valor de r: ")) * 1000
-v_e = math.sqrt(2 * g * r)
-print(f"La velocidad de escape es {v_e:.1f} [m/s] ")
